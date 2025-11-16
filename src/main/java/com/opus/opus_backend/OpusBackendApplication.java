8f7cb9a7c6d0c40c60dd5bd1180cb347ba64015a
JJimini <min102602@pusan.ac.kr>
2025-11-16 22:52:15 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/opus/opus_backend/OpusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.opus.opus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpusBackendApplication.class, args);
+	}
+
+}
