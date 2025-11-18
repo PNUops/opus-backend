d80b5457a22ca03410c9d6a6e3cedb140e09dc51
JJimini <min102602@pusan.ac.kr>
2025-11-18 23:10:22 +0900

    feat: global - config 코드 작성

---
+++ src/main/java/com/opus/opus/global/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.opus.opus.global.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value(value = "${cors.allow.origins}")
+    private String[] allowedOrigins;
+
+    @Value(value = "${cors.allow.methods}")
+    private String[] allowedMethods;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
