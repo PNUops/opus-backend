9a0d50af5bb03ad9c23c8e13350bd5b13e6ef123
myeowon <myw7931@gmail.com>
2025-11-26 15:23:51 +0900

    feat : 대회 컨트롤러 구현

---
+++ src/main/java/com/opus/opus/modules/contest/application/dto/request/ContestRequest.java
@@ -0,0 +1,9 @@
+package com.opus.opus.modules.contest.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ContestRequest(
+        @NotBlank(message = "대회명은 비어 있을 수 없습니다.")
+        String contestName
+) {
+}
