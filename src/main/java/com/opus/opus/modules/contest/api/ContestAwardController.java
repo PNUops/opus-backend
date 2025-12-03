ca1adff8c4519ff52b09f12ca77c80a4763e5382
pykido <animoto1@naver.com>
2025-12-03 17:29:15 +0900

    refactor : 대회 수상 수정 API Patch로 수정

--- src/main/java/com/opus/opus/modules/contest/api/ContestAwardController.java
+++ src/main/java/com/opus/opus/modules/contest/api/ContestAwardController.java
@@ -13,9 +13,9 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +45,7 @@ public class ContestAwardController {
         return ResponseEntity.ok(responses);
     }
 
-    @PutMapping("/{awardId}")
+    @PatchMapping("/{awardId}")
     public ResponseEntity<Void> updateContestAward(
             @PathVariable Long contestId,
             @PathVariable Long awardId,
