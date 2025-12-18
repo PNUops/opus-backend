6d207b94976b959454a6a38bef9d6531a11213cf
JJimini <min102602@pusan.ac.kr>
2025-12-18 20:26:04 +0900

    docs: 로그인 API 문서화

--- src/test/java/com/opus/opus/restdocs/docs/MemberApiDocsTest.java
+++ src/test/java/com/opus/opus/restdocs/docs/MemberApiDocsTest.java
@@ -2,38 +2,48 @@ package com.opus.opus.restdocs.docs;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.opus.opus.member.MemberFixture;
 import com.opus.opus.modules.member.application.dto.request.EmailAuthConfirmRequest;
 import com.opus.opus.modules.member.application.dto.request.EmailAuthRequest;
+import com.opus.opus.modules.member.application.dto.request.SignInRequest;
 import com.opus.opus.modules.member.application.dto.request.SignUpRequest;
+import com.opus.opus.modules.member.application.dto.response.SignInResponse;
 import com.opus.opus.modules.member.domain.Member;
 import com.opus.opus.restdocs.RestDocsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 
 public class MemberApiDocsTest extends RestDocsTest {
 
     private Member member;
+    @Autowired
+    private RestDocumentationResultHandler restDocs;
 
     @BeforeEach
     void setUp() {
         this.member = MemberFixture.createMember();
+        setField(member, "id", 1L);
     }
 
     @Test
     @DisplayName("[성공] 유효한 요청이면 회원가입 이메일 인증 코드는 발송된다.")
     void 유효한_요청이면_회원가입_이메일_인증_코드는_발송된다() throws Exception {
-        doNothing().when(memberCommandService).signUpEmailAuth(any());
-
         final EmailAuthRequest request = new EmailAuthRequest(member.getEmail());
+
+        doNothing().when(memberCommandService).signUpEmailAuth(any());
 
         mockMvc.perform(post("/sign-up/email-auth")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -49,9 +59,9 @@ public class MemberApiDocsTest extends RestDocsTest {
     @Test
     @DisplayName("[성공] 정상적으로 이메일 인증코드를 확인할 수 있다.")
     void 정상적으로_이메일_인증코드를_확인할_수_있다() throws Exception {
-        doNothing().when(memberCommandService).confirmSignUpEmailAuth(any());
-
         final EmailAuthConfirmRequest request = new EmailAuthConfirmRequest(member.getEmail(), "exampleCode");
+
+        doNothing().when(memberCommandService).confirmSignUpEmailAuth(any());
 
         mockMvc.perform(patch("/sign-up/email-auth")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -68,10 +78,11 @@ public class MemberApiDocsTest extends RestDocsTest {
     @Test
     @DisplayName("[성공] 유효한 요청이면 회원가입은 정상적으로 이뤄진다.")
     void 유효한_요청이면_회원가입은_정상적으로_이뤄진다() throws Exception {
-        doNothing().when(memberCommandService).signUp(any());
-
         final SignUpRequest request = new SignUpRequest(member.getName(), member.getStudentId(), member.getEmail(),
                 "qwer123!");
+
+        doNothing().when(memberCommandService).signUp(any());
+
         mockMvc.perform(post("/sign-up")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
@@ -82,6 +93,33 @@ public class MemberApiDocsTest extends RestDocsTest {
                                 stringFieldWithPath("studentId", "회원의 학번"),
                                 stringFieldWithPath("email", "회원의 이메일"),
                                 stringFieldWithPath("password", "회원의 비밀번호")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("[성공] 유효한 요청이면 로그인은 정상적으로 이뤄진다.")
+    void 유효한_요청이면_로그인은_정상적으로_이뤄진다() throws Exception {
+        final SignInResponse response = new SignInResponse(member.getId(), member.getName(), "exampleToken",
+                member.getRoles());
+        final SignInRequest request = new SignInRequest(member.getEmail(), "qwer123!");
+
+        when(memberCommandService.signIn(any())).thenReturn(response);
+
+        mockMvc.perform(post("/sign-in")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(document("signin",
+                        requestFields(
+                                stringFieldWithPath("email", "로그인 이메일"),
+                                stringFieldWithPath("password", "비밀번호 (영문+숫자+특수문자 조합)")
+                        ),
+                        responseFields(
+                                numberFieldWithPath("memberId", "회원 고유 식별자"),
+                                stringFieldWithPath("name", "회원 이름"),
+                                stringFieldWithPath("token", "JWT 액세스 토큰"),
+                                arrayFieldWithPath("types", "회원 권한 목록(회원, 관리자)")
                         )
                 ));
     }
