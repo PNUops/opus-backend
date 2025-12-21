4ad9426ef267acda3c54227edb91c47041715e2e
JJimini <min102602@pusan.ac.kr>
2025-12-21 22:11:44 +0900

    feat: 전체 공지사항 개발 API 개발

--- src/main/java/com/opus/opus/modules/notice/api/NoticeController.java
+++ src/main/java/com/opus/opus/modules/notice/api/NoticeController.java
@@ -1,11 +1,30 @@
 package com.opus.opus.modules.notice.api;
 
+import com.opus.opus.modules.notice.application.NoticeCommandService;
+import com.opus.opus.modules.notice.application.NoticeQueryService;
+import com.opus.opus.modules.notice.application.dto.request.NoticeRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
 public class NoticeController {
+
+    private final NoticeCommandService noticeCommandService;
+    private final NoticeQueryService noticeQueryService;
+
+    @PostMapping("/notices")
+    @Secured("ROLE_관리자")
+    public ResponseEntity<Void> createNotice(@Valid @RequestBody final NoticeRequest request) {
+        noticeCommandService.createNotice(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
--- src/main/java/com/opus/opus/modules/notice/application/NoticeCommandService.java
+++ src/main/java/com/opus/opus/modules/notice/application/NoticeCommandService.java
@@ -1,5 +1,8 @@
 package com.opus.opus.modules.notice.application;
 
+import com.opus.opus.modules.notice.application.dto.request.NoticeRequest;
+import com.opus.opus.modules.notice.domain.Notice;
+import com.opus.opus.modules.notice.domain.dao.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class NoticeCommandService {
+
+    private final NoticeRepository noticeRepository;
+
+    public void createNotice(final NoticeRequest request) {
+        noticeRepository.save(Notice.builder()
+                .title(request.title())
+                .description(request.description())
+                .build());
+    }
 }
