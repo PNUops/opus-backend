fbb00539e50fd26e0ec97e52c2720313839e522e
JJimini <min102602@pusan.ac.kr>
2025-12-22 17:13:11 +0900

    test: 전체 공지사항 수정 테스트 작성

--- src/test/java/com/opus/opus/notice/application/NoticeCommandServiceTest.java
+++ src/test/java/com/opus/opus/notice/application/NoticeCommandServiceTest.java
@@ -7,6 +7,8 @@ import com.opus.opus.modules.notice.application.NoticeCommandService;
 import com.opus.opus.modules.notice.application.dto.request.NoticeRequest;
 import com.opus.opus.modules.notice.domain.Notice;
 import com.opus.opus.modules.notice.domain.dao.NoticeRepository;
+import com.opus.opus.notice.NoticeFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,13 @@ public class NoticeCommandServiceTest extends IntegrationTest {
     @Autowired
     private NoticeRepository noticeRepository;
 
+    private Notice notice;
+
+    @BeforeEach
+    void setUp() {
+        notice = noticeRepository.save(NoticeFixture.createNotice());
+    }
+
     @Test
     @DisplayName("[성공] 전체 공지사항이 정상적으로 생성된다.")
     void 전체_공지사항이_정상적으로_생성된다() {
@@ -30,5 +39,18 @@ public class NoticeCommandServiceTest extends IntegrationTest {
         assertThat(notice.getTitle()).isEqualTo(request.title());
         assertThat(notice.getDescription()).isEqualTo(request.description());
         assertThat(notice.getContestId()).isNull();
+    }
+
+    @Test
+    @DisplayName("[성공] 전체 공지사항이 정상적으로 수정된다.")
+    void 전체_공지사항이_정상적으로_수정된다() {
+        final NoticeRequest request = new NoticeRequest("수정 공지 제목", "수정 공지 내용");
+        assertThat(notice.getTitle()).isEqualTo(notice.getTitle());
+
+        noticeCommandService.updateNotice(request, notice.getId());
+
+        final Notice updateNotice = noticeRepository.findAllByOrderByCreatedAtDesc().get(0);
+        assertThat(updateNotice.getTitle()).isEqualTo(request.title());
+        assertThat(updateNotice.getDescription()).isEqualTo(request.description());
     }
 }
