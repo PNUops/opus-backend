667f0f619b5f8125cb639c3244ed6c4c6e6759e5
JJimini <min102602@pusan.ac.kr>
2025-12-22 18:00:31 +0900

    test: 전체 공지사항 목록 조회 테스트 작성

--- src/test/java/com/opus/opus/notice/application/NoticeQueryServiceTest.java
+++ src/test/java/com/opus/opus/notice/application/NoticeQueryServiceTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.opus.opus.helper.IntegrationTest;
 import com.opus.opus.modules.notice.application.NoticeQueryService;
 import com.opus.opus.modules.notice.application.dto.response.NoticeDetailResponse;
+import com.opus.opus.modules.notice.application.dto.response.NoticeSummaryResponse;
 import com.opus.opus.modules.notice.domain.Notice;
 import com.opus.opus.modules.notice.domain.dao.NoticeRepository;
 import com.opus.opus.notice.NoticeFixture;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,6 +29,7 @@ public class NoticeQueryServiceTest extends IntegrationTest {
     void setUp() {
         notice = noticeRepository.save(NoticeFixture.createNotice());
     }
+
     @Test
     @DisplayName("[성공] 전체 공지사항을 상세 조회할 수 있다.")
     void 전체_공지사항을_상세_조회할_수_있다() {
@@ -34,5 +37,18 @@ public class NoticeQueryServiceTest extends IntegrationTest {
 
         assertThat(response.title()).isEqualTo(notice.getTitle());
         assertThat(response.description()).isEqualTo(notice.getDescription());
+    }
+
+    @Test
+    @DisplayName("[성공] 전체 공지사항 목록을 조회할 수 있다.")
+    void 전체_공지사항_목록을_조회할_수_있다() {
+        final Notice anotherNotice = noticeRepository.save(NoticeFixture.createNotice());
+
+        final List<NoticeSummaryResponse> responses = noticeQueryService.getAllNotices();
+
+        assertThat(responses).hasSize(2);
+        assertThat(responses)
+                .extracting(NoticeSummaryResponse::noticeId)
+                .containsExactlyInAnyOrder(notice.getId(), anotherNotice.getId());
     }
 }
