7efa1a8e88f411b8a1bd71a05eedf4b7aea13d2a
JJimini <min102602@pusan.ac.kr>
2025-12-22 17:47:23 +0900

    test: 전체 공지사항 상세 조회 테스트 작성

---
+++ src/test/java/com/opus/opus/notice/application/NoticeQueryServiceTest.java
@@ -0,0 +1,38 @@
+package com.opus.opus.notice.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.opus.opus.helper.IntegrationTest;
+import com.opus.opus.modules.notice.application.NoticeQueryService;
+import com.opus.opus.modules.notice.application.dto.response.NoticeDetailResponse;
+import com.opus.opus.modules.notice.domain.Notice;
+import com.opus.opus.modules.notice.domain.dao.NoticeRepository;
+import com.opus.opus.notice.NoticeFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class NoticeQueryServiceTest extends IntegrationTest {
+
+    @Autowired
+    private NoticeQueryService noticeQueryService;
+
+    @Autowired
+    private NoticeRepository noticeRepository;
+
+    private Notice notice;
+
+    @BeforeEach
+    void setUp() {
+        notice = noticeRepository.save(NoticeFixture.createNotice());
+    }
+    @Test
+    @DisplayName("[성공] 전체 공지사항을 상세 조회할 수 있다.")
+    void 전체_공지사항을_상세_조회할_수_있다() {
+        final NoticeDetailResponse response = noticeQueryService.getNotice(notice.getId());
+
+        assertThat(response.title()).isEqualTo(notice.getTitle());
+        assertThat(response.description()).isEqualTo(notice.getDescription());
+    }
+}
