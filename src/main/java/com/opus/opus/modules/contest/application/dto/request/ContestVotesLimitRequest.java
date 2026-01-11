46b9c5aec63b2be5ccadb9c7d8ba8f91b7bd27dc
pykido <animoto1@naver.com>
2026-01-11 21:05:00 +0900

    feat : 최대 투표 개수를 0 이상으로 설정

--- src/main/java/com/opus/opus/modules/contest/application/dto/request/ContestVotesLimitRequest.java
+++ src/main/java/com/opus/opus/modules/contest/application/dto/request/ContestVotesLimitRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record ContestVotesLimitRequest(
         @NotNull(message = "최대 투표 개수는 필수입니다.")
-        @Min(value = 1, message = "최대 투표 개수는 1 이상이어야 합니다.")
+        @Min(value = 0, message = "최대 투표 개수는 0 이상이어야 합니다.")
         Integer maxVotesLimit
 ) {
 }
