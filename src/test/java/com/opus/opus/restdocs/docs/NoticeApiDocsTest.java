bb667328903bed533cc145de7c220b8491ae45be
JJimini <min102602@pusan.ac.kr>
2026-01-12 23:45:29 +0900

    docs: 대회별 공지 상세 조회 문서화

--- src/test/java/com/opus/opus/restdocs/docs/NoticeApiDocsTest.java
+++ src/test/java/com/opus/opus/restdocs/docs/NoticeApiDocsTest.java
@@ -112,11 +112,11 @@ public class NoticeApiDocsTest extends RestDocsTest {
     @Test
     @DisplayName("[성공] 유효한 요청이면 정상적으로 전체 공지사항 상세 조회를 할 수 있다.")
     void 유효한_요청이면_정상적으로_전체_공지사항_상세_조회를_할_수_있다() throws Exception {
-        final NoticeDetailResponse response = new NoticeDetailResponse("공지 전체 제목", "공지 전체 내용", now(), now());
+        final NoticeDetailResponse response = new NoticeDetailResponse("전체 공지 제목", "전체 공지 내용", now(), now());
 
         when(noticeQueryService.getNotice(any())).thenReturn(response);
 
-        mockMvc.perform(get("/notices/{noticeId}", 1L))
+        mockMvc.perform(get("/notices/{noticeId}", 1))
                 .andExpect(status().isOk())
                 .andDo(document("get-notice",
                         pathParameters(
@@ -211,7 +211,7 @@ public class NoticeApiDocsTest extends RestDocsTest {
     void 유효한_요청이면_정상적으로_대회별_공지사항이_삭제된다() throws Exception {
         doNothing().when(noticeCommandService).deleteContestNotice(any(), any());
 
-        mockMvc.perform(delete("/contests/{contestId}/notices/{noticeId}", 1,1)
+        mockMvc.perform(delete("/contests/{contestId}/notices/{noticeId}", 1, 1)
                         .header(HttpHeaders.AUTHORIZATION, ADMIN_TOKEN))
                 .andExpect(status().isNoContent())
                 .andDo(document("delete-contest-notice",
@@ -221,6 +221,29 @@ public class NoticeApiDocsTest extends RestDocsTest {
                         ),
                         requestHeaders(
                                 headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer {accessToken} (관리자)")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("[성공] 유효한 요청이면 정상적으로 대회별 공지사항 상세 조회를 할 수 있다.")
+    void 유효한_요청이면_정상적으로_대회별_공지사항_상세_조회를_할_수_있다() throws Exception {
+        final NoticeDetailResponse response = new NoticeDetailResponse("대회별 공지 제목", "대회별 공지 내용", now(), now());
+
+        when(noticeQueryService.getContestNotice(any(), any())).thenReturn(response);
+
+        mockMvc.perform(get("/contests/{contestId}/notices/{noticeId}", 1, 1))
+                .andExpect(status().isOk())
+                .andDo(document("get-contest-notice",
+                        pathParameters(
+                                parameterWithName("contestId").description("대회 ID"),
+                                parameterWithName("noticeId").description("공지 ID")
+                        ),
+                        responseFields(
+                                stringFieldWithPath("title", "공지 제목"),
+                                stringFieldWithPath("description", "공지 내용"),
+                                dateTimeFieldWithPath("createdAt", "공지 생성 시각"),
+                                dateTimeFieldWithPath("updatedAt", "공지 수정 시각")
                         )
                 ));
     }
