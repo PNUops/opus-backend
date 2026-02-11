62d21df882a6ede0096c186616b5a79edb74e917
sjmoon00 <paulmoon00@naver.com>
2026-02-11 19:14:20 +0900

    feat: 분과 기본 썸네일 등록 기능 구현

--- src/main/java/com/opus/opus/modules/contest/application/ContestTrackCommandService.java
+++ src/main/java/com/opus/opus/modules/contest/application/ContestTrackCommandService.java
@@ -1,15 +1,22 @@
 package com.opus.opus.modules.contest.application;
 
+import static com.opus.opus.modules.file.domain.FileImageType.THUMBNAIL;
+import static com.opus.opus.modules.file.domain.ReferenceDomainType.TRACK;
+
+import com.opus.opus.global.util.FileStorageUtil;
 import com.opus.opus.modules.contest.application.convenience.ContestConvenience;
 import com.opus.opus.modules.contest.application.convenience.ContestTrackConvenience;
 import com.opus.opus.modules.contest.application.dto.request.ContestTrackRequest;
 import com.opus.opus.modules.contest.domain.Contest;
 import com.opus.opus.modules.contest.domain.ContestTrack;
 import com.opus.opus.modules.contest.domain.dao.ContestTrackRepository;
+import com.opus.opus.modules.file.domain.FileImageType;
+import com.opus.opus.modules.file.domain.dao.FileRepository;
 import com.opus.opus.modules.team.application.convenience.TeamConvenience;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional
@@ -17,10 +24,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class ContestTrackCommandService {
 
     private final ContestTrackRepository contestTrackRepository;
+    private final FileRepository fileRepository;
 
     private final ContestTrackConvenience contestTrackConvenience;
     private final ContestConvenience contestConvenience;
     private final TeamConvenience teamConvenience;
+
+    private final FileStorageUtil fileStorageUtil;
 
     public void createTrack(final Long contestId, final ContestTrackRequest request) {
         final Contest contest = contestConvenience.getValidateExistContest(contestId);
@@ -43,5 +53,16 @@ public class ContestTrackCommandService {
         final ContestTrack contestTrack = contestTrackConvenience.getValidateExistTrack(contestId, trackId);
         teamConvenience.validateAllTeamsDeletedInTrack(trackId);
         contestTrackRepository.delete(contestTrack);
+    }
+
+    public void saveContestTrackDefaultThumbnail(final Long contestId, final Long trackId, final MultipartFile image) {
+        contestTrackConvenience.getValidateExistTrack(contestId, trackId);
+        deleteIfExists(trackId, THUMBNAIL);
+        fileStorageUtil.storeFile(image, trackId, TRACK, THUMBNAIL);
+    }
+
+    private void deleteIfExists(final Long trackId, final FileImageType imageType) {
+        fileRepository.findByReferenceIdAndReferenceTypeAndImageType(trackId, TRACK, imageType)
+                .ifPresent(existingFile -> fileStorageUtil.deleteFile(existingFile.getId()));
     }
 }
