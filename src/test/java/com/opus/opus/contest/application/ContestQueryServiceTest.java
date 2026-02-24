c5f33964f427f5c47aaedb812065699ca697b74c
myeowon <myw7931@gmail.com>
2026-02-25 01:01:59 +0900

    feat : 대회 전체 팀 조회 serviceTest 작성

--- src/test/java/com/opus/opus/contest/application/ContestQueryServiceTest.java
+++ src/test/java/com/opus/opus/contest/application/ContestQueryServiceTest.java
@@ -10,19 +10,19 @@ import com.opus.opus.helper.IntegrationTest;
 import com.opus.opus.member.MemberFixture;
 import com.opus.opus.modules.contest.application.ContestQueryService;
 import com.opus.opus.modules.contest.application.dto.response.ContestVotesLimitResponse;
-import com.opus.opus.modules.contest.application.dto.response.VotePeriodResponse;
 import com.opus.opus.modules.contest.application.dto.response.TeamSummaryResponse;
+import com.opus.opus.modules.contest.application.dto.response.VotePeriodResponse;
 import com.opus.opus.modules.contest.domain.Contest;
 import com.opus.opus.modules.contest.domain.ContestCategory;
 import com.opus.opus.modules.contest.domain.dao.ContestCategoryRepository;
 import com.opus.opus.modules.contest.domain.dao.ContestRepository;
 import com.opus.opus.modules.contest.exception.ContestException;
-import java.time.LocalDateTime;
 import com.opus.opus.modules.member.domain.Member;
 import com.opus.opus.modules.member.domain.dao.MemberRepository;
 import com.opus.opus.modules.team.domain.Team;
 import com.opus.opus.modules.team.domain.dao.TeamRepository;
 import com.opus.opus.team.TeamFixture;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -53,7 +53,7 @@ public class ContestQueryServiceTest extends IntegrationTest {
     @BeforeEach
     void setUp() {
         category = contestCategoryRepository.save(ContestCategoryFixture.createContestCategory());
-        contest = contestRepository.save(ContestFixture.createContest(category.getId()));
+        contest = contestRepository.save(ContestFixture.createContestWithCategoryId(category.getId()));
         member = memberRepository.save(MemberFixture.createMember());
     }
 
@@ -102,8 +102,8 @@ public class ContestQueryServiceTest extends IntegrationTest {
     @Test
     @DisplayName("[성공] 대회의 팀 목록을 조회할 수 있다.")
     void 대회의_팀_목록을_조회할_수_있다() {
-        final Team team1 = teamRepository.save(TeamFixture.createTeam(contest.getId()));
-        final Team team2 = teamRepository.save(TeamFixture.createTeam(contest.getId()));
+        final Team team1 = teamRepository.save(TeamFixture.createTeamWithContestId(contest.getId()));
+        final Team team2 = teamRepository.save(TeamFixture.createTeamWithContestId(contest.getId()));
 
         final List<TeamSummaryResponse> responses = contestQueryService.getContestTeamSummaries(contest.getId(),
                 member);
