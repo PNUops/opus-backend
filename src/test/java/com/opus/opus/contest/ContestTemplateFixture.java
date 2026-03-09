66ba4b721f1147dae30054be9c684f177f202c86
JJimini <min102602@pusan.ac.kr>
2026-03-09 11:22:09 +0900

    fix: ContestTemplateFixture의 파라미터 받는 방식 변경

--- src/test/java/com/opus/opus/contest/ContestTemplateFixture.java
+++ src/test/java/com/opus/opus/contest/ContestTemplateFixture.java
@@ -1,25 +1,20 @@
 package com.opus.opus.contest;
 
+import com.opus.opus.modules.contest.application.dto.request.ContestTemplateRequest;
 import com.opus.opus.modules.contest.domain.Contest;
 import com.opus.opus.modules.contest.domain.ContestTemplate;
 
 public class ContestTemplateFixture {
 
     public static ContestTemplate createContestTemplate(final Contest contest) {
+        final ContestTemplateRequest request = new ContestTemplateRequest(
+                true, true, true, true, true, true,
+                true, true, true, true, true, true
+        );
+
         return ContestTemplate.builder()
                 .contest(contest)
-                .trackRequired(true)
-                .projectNameRequired(true)
-                .teamNameRequired(true)
-                .leaderRequired(true)
-                .teamMembersRequired(true)
-                .professorRequired(true)
-                .githubPathRequired(true)
-                .youTubePathRequired(true)
-                .productionPathRequired(true)
-                .overviewRequired(true)
-                .posterRequired(true)
-                .imagesRequired(true)
+                .request(request)
                 .build();
     }
 }
