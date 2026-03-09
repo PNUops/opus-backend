0463ce8456522f314a5a69cdadf1ca595622375e
pykido <animoto1@naver.com>
2026-03-09 20:12:13 +0900

    feat : 학번 패턴 추가

--- src/main/java/com/opus/opus/modules/member/application/dto/request/StudentIdUpdateRequest.java
+++ src/main/java/com/opus/opus/modules/member/application/dto/request/StudentIdUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.opus.opus.modules.member.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record StudentIdUpdateRequest(
 
         @NotBlank(message = "학번을 입력해주세요.")
+        @Pattern(regexp = "^\\d{9}$", message = "학번은 9자리 숫자여야 합니다.")
         String studentId
 ) {
 }
