35dd5836f1ff389aa3768f10b34cbfc6b230fff6
sjmoon00 <paulmoon00@naver.com>
2026-03-12 12:03:35 +0900

    feat: 사이드바 조회 기능구현

--- src/main/java/com/opus/opus/modules/contest/application/ContestCategoryQueryService.java
+++ src/main/java/com/opus/opus/modules/contest/application/ContestCategoryQueryService.java
@@ -1,9 +1,14 @@
 package com.opus.opus.modules.contest.application;
 
 import com.opus.opus.modules.contest.application.dto.response.ContestCategoryResponse;
+import com.opus.opus.modules.contest.application.dto.response.SidebarResponse;
+import com.opus.opus.modules.contest.domain.Contest;
 import com.opus.opus.modules.contest.domain.ContestCategory;
 import com.opus.opus.modules.contest.domain.dao.ContestCategoryRepository;
+import com.opus.opus.modules.contest.domain.dao.ContestRepository;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +19,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class ContestCategoryQueryService {
 
     private final ContestCategoryRepository contestCategoryRepository;
+    private final ContestRepository contestRepository;
 
     public List<ContestCategoryResponse> getAllContestCategories() {
         List<ContestCategory> ContestCategories = contestCategoryRepository.findAll();
 
         return ContestCategories.stream()
                 .map(ContestCategoryResponse::from)
+                .toList();
+    }
+
+    public List<SidebarResponse> getSidebar() {
+        final List<ContestCategory> categories = contestCategoryRepository.findAll();
+        final Map<Long, List<Contest>> contestsByCategory = contestRepository.findAll().stream()
+                .collect(Collectors.groupingBy(Contest::getCategoryId));
+
+        return categories.stream()
+                .map(category -> SidebarResponse.of(category, contestsByCategory.getOrDefault(category.getId(), List.of())))
                 .toList();
     }
 }
