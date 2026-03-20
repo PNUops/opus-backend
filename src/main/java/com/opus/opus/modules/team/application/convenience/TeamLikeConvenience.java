2ccd18086b09c70575b13f6dcf147445a6ba7f83
myeowon <myw7931@gmail.com>
2026-03-20 16:12:21 +0900

    refactor : 투표/좋아요 조회 로직 리팩토링 (네이밍 규칙 및 구조 통일)

--- src/main/java/com/opus/opus/modules/team/application/convenience/TeamLikeConvenience.java
+++ src/main/java/com/opus/opus/modules/team/application/convenience/TeamLikeConvenience.java
@@ -3,6 +3,7 @@ package com.opus.opus.modules.team.application.convenience;
 import static java.util.stream.Collectors.toMap;
 
 import com.opus.opus.modules.member.domain.Member;
+import com.opus.opus.modules.team.domain.Team;
 import com.opus.opus.modules.team.domain.TeamLike;
 import com.opus.opus.modules.team.domain.dao.TeamLikeRepository;
 import java.util.Map;
@@ -17,13 +18,30 @@ public class TeamLikeConvenience {
 
     private final TeamLikeRepository teamLikeRepository;
 
-    public Map<Long, Boolean> getLikeMap(final Long contestId, final Member member) {
-        return teamLikeRepository.findAllByMemberIdAndContestId(member.getId(), contestId).stream()
-                .collect(toMap(tl -> tl.getTeam().getId(), TeamLike::getIsLiked));
+    // 1. 대회 내 모든 팀 맵 조회
+    public Map<Long, Boolean> getLikeMapIfInPeriod(final Long contestId, final Member member,
+                                                   final boolean isVotingPeriod) {
+        return (member != null && isVotingPeriod)
+                ? getLikeMapByContestId(contestId, member)
+                : Map.of();
     }
 
-    public Map<Long, Boolean> getLikeMapIfNotVotingPeriod(final Long contestId, final Member member,
-                                                          final boolean isVotingPeriod) {
-        return (member != null && !isVotingPeriod) ? getLikeMap(contestId, member) : Map.of();
+    private Map<Long, Boolean> getLikeMapByContestId(final Long contestId, final Member member) {
+        return teamLikeRepository.findAllByMemberIdAndContestId(member.getId(), contestId).stream()
+                .collect(toMap(
+                        tv -> tv.getTeam().getId(),
+                        TeamLike::getIsLiked
+                ));
+    }
+
+    // 2. 단일 팀 투표 여부 조회
+    public boolean getIsLikedIfInPeriod(final Team team, final Member member, final boolean isVotingPeriod) {
+        return member != null && !isVotingPeriod && getIsLikedByTeamAndMember(team, member);
+    }
+
+    private boolean getIsLikedByTeamAndMember(final Team team, final Member member) {
+        return teamLikeRepository.findByMemberIdAndTeam(member.getId(), team)
+                .map(TeamLike::getIsLiked)
+                .orElse(false);
     }
 }
