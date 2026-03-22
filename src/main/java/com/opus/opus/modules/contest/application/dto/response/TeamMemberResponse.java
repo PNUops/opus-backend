e6fb167c05449fec3baf45e9c81156a2e2a31c0f
myeowon <myw7931@gmail.com>
2026-03-22 16:00:22 +0900

    feat : 팀장/팀원 정보 response에서 팀원의 roles에 ROLE_팀장이 있다면 ROLE_팀장를 반환하도록 로직 변경

--- src/main/java/com/opus/opus/modules/contest/application/dto/response/TeamMemberResponse.java
+++ src/main/java/com/opus/opus/modules/contest/application/dto/response/TeamMemberResponse.java
@@ -3,7 +3,6 @@ package com.opus.opus.modules.contest.application.dto.response;
 import com.opus.opus.modules.member.domain.Member;
 import com.opus.opus.modules.team.domain.TeamMember;
 import com.opus.opus.modules.team.domain.TeamMemberRoleType;
-import java.util.Set;
 
 public record TeamMemberResponse(
         Long teamMemberId,
@@ -11,10 +10,14 @@ public record TeamMemberResponse(
         TeamMemberRoleType roleType
 ) {
     public static TeamMemberResponse of(final TeamMember teamMember, final Member member) {
+        TeamMemberRoleType role = teamMember.getRoles().contains(TeamMemberRoleType.ROLE_팀장)
+                ? TeamMemberRoleType.ROLE_팀장
+                : TeamMemberRoleType.ROLE_팀원;
+
         return new TeamMemberResponse(
                 member.getId(),
                 member.getName(),
-                teamMember.getRoles().stream().findFirst().orElse(TeamMemberRoleType.ROLE_팀원)
+                role
         );
     }
 }
