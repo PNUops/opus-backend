b8ac3aaaf93ba1ccc0faac086784dcf12099413a
myeowon <myw7931@gmail.com>
2026-03-26 15:19:48 +0900

    fix : rollback

--- src/main/java/com/opus/opus/modules/team/domain/dao/TeamMemberRepository.java
+++ src/main/java/com/opus/opus/modules/team/domain/dao/TeamMemberRepository.java
@@ -1,7 +1,6 @@
 package com.opus.opus.modules.team.domain.dao;
 
 import com.opus.opus.modules.team.domain.TeamMember;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +9,4 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
     boolean existsByTeamIdAndMemberId(final Long teamId, final Long memberId);
 
     Optional<TeamMember> findByTeamIdAndMemberId(final Long teamId, final Long memberId);
-
-    List<TeamMember> findAllByMemberId(final Long memberId);
 }
