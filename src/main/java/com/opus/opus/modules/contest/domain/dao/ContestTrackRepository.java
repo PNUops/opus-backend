3387d39e63b7b148d68fcf9d159975bc4562833a
myeowon <myw7931@gmail.com>
2026-03-30 19:30:27 +0900

    feat : 유효하지 않은 contestId로 트랙 조회 요청 시 예외 처리

--- src/main/java/com/opus/opus/modules/contest/domain/dao/ContestTrackRepository.java
+++ src/main/java/com/opus/opus/modules/contest/domain/dao/ContestTrackRepository.java
@@ -2,13 +2,10 @@ package com.opus.opus.modules.contest.domain.dao;
 
 import com.opus.opus.modules.contest.domain.ContestTrack;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContestTrackRepository extends JpaRepository<ContestTrack, Long> {
     boolean existsByContestIdAndTrackName(final Long contestId, final String trackName);
-
-    Optional<ContestTrack> findByIdAndContestId(final Long trackId, final Long contestId);
 
     List<ContestTrack> findAllByContestId(final Long contestId);
 }
