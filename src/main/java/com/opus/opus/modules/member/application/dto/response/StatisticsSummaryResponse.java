b1adaedbffbc35021b7180f72c8eabdd7b1e16fb
sjmoon00 <paulmoon00@naver.com>
2026-05-06 00:26:17 +0900

    feat: 통계 응답 수정

--- src/main/java/com/opus/opus/modules/member/application/dto/response/StatisticsSummaryResponse.java
+++ src/main/java/com/opus/opus/modules/member/application/dto/response/StatisticsSummaryResponse.java
@@ -1,6 +1,7 @@
 package com.opus.opus.modules.member.application.dto.response;
 
 public record StatisticsSummaryResponse(
+        long totalMembers,
         long totalProjects,
         long totalLikes,
         long totalContests
