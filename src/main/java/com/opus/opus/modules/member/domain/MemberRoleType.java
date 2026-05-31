70cf7955f0a2f9fa1cbf369ca9465f5d996af052
JJimini <min102602@pusan.ac.kr>
2026-05-31 21:54:21 +0900

    refactor: memberRoleType 변경

--- src/main/java/com/opus/opus/modules/member/domain/MemberRoleType.java
+++ src/main/java/com/opus/opus/modules/member/domain/MemberRoleType.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum MemberRoleType {
-    ROLE_회원(1),
+    ROLE_학생(1),
     ROLE_관리자(2),
+    ROLE_교수(3),
+    ROLE_직원(4),
+    ROLE_외부멘토(5),
     ;
 
     private final long id;
